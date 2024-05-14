76d5701fcff90ec7d63a2e32fb55ba1031c97e08
User-xxU000 <158011297+User-xxU000@users.noreply.github.com>
2024-05-14 12:49:34 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,16 @@
+import streamlit as st
+st.set_page_config(page_title="Adael Vega Tareas 4º Año D")
+
+col1,col2,col3 = st.columns(3)
+
+with col1:
+	st.title("tareas 4º año D")
+
+file_path = r"C:\Users\user\Desktop\paguina_web_4añod\trabajo escolar (1).pdf"
+with col1:
+	download_setup = st.download_button("Download", open(file_path, "rb"), file_name="trabajo escolar.pdf")
+
+
+
+
+
